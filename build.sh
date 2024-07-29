db70d3558e637d198ca68e23b9feba98da4927c8
Brandon Rozek <brozek@brandonrozek.com>
2024-07-28 20:04:17 -0700

    Sh -> Bash

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Generate toot markdown files
 
@@ -8,7 +8,6 @@ generate_exe="./target/release/generate_md"
 
 # Check if the file exists and is executable
 if [ -e "$generate_exe" ] && [ -x "$generate_exe" ]; then
-    echo "The file '$file_path' exists and is executable."
     ./target/release/generate_md
 else
     echo "The executable '$generate_exe' does not exist."
