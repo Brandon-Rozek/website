4acfd4fc4a003ca47c96749962c92570109620a3
Brandon Rozek <brozek@brandonrozek.com>
2024-11-29 16:27:19 -0500

    Distributed deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-rsync -Pazc --exclude=*.bak --delete public/ build@brandonrozek.com:brandonrozek/
-
+rsync -Pazc --exclude=*.bak --delete public/ build@Rozek-Cloud:brandonrozek/
+rsync -Pazc --exclude=*.bak --delete public/ build@Rozek-Stratus:brandonrozek/
