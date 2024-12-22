f7759b63c93271e09eee24da70d0d5794cae48f7
Brandon Rozek <brozek@brandonrozek.com>
2024-12-21 20:13:54 -0500

    Switched NY server

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
-rsync -Pazc --exclude=*.bak --delete public/ build@Rozek-Cloud:brandonrozek/
+rsync -Pazc --exclude=*.bak --delete public/ build@Rozek-Fog:brandonrozek/
 rsync -Pazc --exclude=*.bak --delete public/ build@Rozek-Stratus:brandonrozek/
+
