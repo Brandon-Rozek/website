c7721dd927bb0fa8a18c9d0f654e56ad33a704ee
Brandon Rozek <brozek@brandonrozek.com>
2025-11-08 13:56:39 -0500

    Reverted deploy dest

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-rsync -Pazc --exclude=*.bak --delete public/ build@Rozek-Nimbus:/var/home/build/brandonrozek/
+rsync -Pazc --exclude=*.bak --delete public/ build@Rozek-Nimbus:brandonrozek/
 
